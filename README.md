08cad8a17c5fad527222072d09ab3a9082ff0378
ryansreenivasam <ryansreenivasam@gmail.com>
2019-09-15 14:19:07 -0500

    initial file upload

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# CS839Assignment 1
+# Ryan Sreenivasam Assignment 1 for CS839
+
+### A simple animated scene.
+
+To build, run the following in the Assignment1 directory:
+`code` cmake CMakeLists.txt
+`code` make
+
+Then navigate to Assignment1/scene/ and run the executable generated
+`code` ./scene
+
+Open the newly generated USD-a file with usdview
+`code` usdview assignment1.usda
---
+++ scene/CMakeFiles/scene.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/ryansreenivasam/Documents/CS839/Assignment1/scene/main.cpp" "/Users/ryansreenivasam/Documents/CS839/Assignment1/scene/CMakeFiles/scene.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "BUILD_COMPONENT_SRC_PREFIX=\"\""
+  "BUILD_OPTLEVEL_DEV"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/opt/local/USD/include"
+  "/Library/Developer/CommandLineTools/SDKs/MacOSX10.14.sdk/usr/include/python2.7"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
